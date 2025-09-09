9f9e04486db503f7a6647ac08727d86677b72d41
RehemaShammah <rehemashammah@gmail.com>
2025-09-09 11:23:58 +0300

    Week 5 Submission

---
+++ answers.sql
@@ -0,0 +1,16 @@
+-- Question 1
+
+DROP INDEX 1dxPhone ON customers;
+
+-- Question 2
+
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+--Question 3
+
+GRANT INSERT ON salesdb.* TO 'bob'@'localhost';
+
+--Question 4
+
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
+
